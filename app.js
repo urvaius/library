c853bae9f1936e5c1243a3c55be4a5d886c97a76
urvaius <urvaius@gmail.com>
2018-05-02 16:25:44 -0700

    next module is setting up nodemon

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/css', express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-app.listen(3000, function(){
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
 });
