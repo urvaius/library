5e796c53e60867199f76345613bfe6c9576d0113
urvaius <urvaius@gmail.com>
2018-05-04 11:25:43 -0700

    setupl next module templating engines.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
@@ -15,6 +15,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-  debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
 });
