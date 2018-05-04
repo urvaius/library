926d2e8e482b413cb7b65d8d31b61fd1aade1af0
urvaius <urvaius@gmail.com>
2018-05-04 11:50:16 -0700

    finished pug examples moving to ejs next so commit here if need to come back to it.

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index', { list: ['a', 'b'] });
 });
 
 app.listen(port, () => {
