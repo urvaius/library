841b79019e878f93178e663400b3c796cb2ef3c1
urvaius <urvaius@gmail.com>
2018-05-04 12:56:11 -0700

    template installed storystrap

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'] });
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => {
