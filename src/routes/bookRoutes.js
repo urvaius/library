f3cf46f76ef075de2725410aa95fa39a3376e758
urvaius <urvaius@gmail.com>
2018-05-10 16:22:52 -0700

    just fixed indent problem

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -24,26 +24,22 @@ function router(nav) {
   ];
   bookRouter.route('/')
     .get((req, res) => {
-      res.render(
-        'bookListView',
-        {
-          nav,
-          title: 'Library',
-          books
-        }
-      );
+      res.render('bookListView', {
+        nav,
+        title: 'Library',
+        books
+      });
     });
   bookRouter.route('/:id')
     .get((req, res) => {
-      const { id } = req.params;
-      res.render(
-        'bookView',
-        {
-          nav,
-          title: 'Library',
-          book: books[id]
-        }
-      );
+      const {
+        id
+      } = req.params;
+      res.render('bookView', {
+        nav,
+        title: 'Library',
+        book: books[id]
+      });
     });
   return bookRouter;
 }
