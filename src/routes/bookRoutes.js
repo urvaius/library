edd5de9134f37f1ee4a33893e4d7e8e81f99aa22
urvaius <urvaius@gmail.com>
2018-05-10 14:39:24 -0700

    done with sql part mongo is next so go back to this if want to use sql

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const sql = require('mssql');
+
 const bookRouter = express.Router();
 const debug = require('debug')('app:bookRoutes');
+
 function router(nav) {
   const books = [{
     title: 'war and peace',
@@ -24,32 +26,35 @@ function router(nav) {
   ];
   bookRouter.route('/')
     .get((req, res) => {
-      const request = new sql.Request();
-
-      request.query('select * from books')
-        .then((result) => {
-          debug(result);
-          res.render(
-            'bookListView',
-            {
-              nav,
-              title: 'Library',
-              books: result.recordset
-            }
-          );
-        });
-    });
-  bookRouter.route('/:id')
-    .get((req, res) => {
-      const { id } = req.params;
-      res.render(
-        'bookView',
-        {
+      (async function query() {
+        const request = new sql.Request();
+        const { recordset } = await request.query('select * from books');
+        //  debug(result);
+        res.render('bookListView', {
           nav,
           title: 'Library',
-          book: books[id]
-        }
-      );
+          books: recordset
+        });
+      }());
+    });
+  bookRouter.route('/:id')
+    .all((req, res, next) => {
+      (async function query() {
+        const { id } = req.params;
+        const request = new sql.Request();
+        const { recordset } = await request.input('id', sql.Int, id)
+          .query('select * from books where id = @id');
+        // req.book = recordset[0];
+        [req.book] = recordset;
+        next();
+      }());
+    })
+    .get((req, res) => {
+      res.render('bookView', {
+        nav,
+        title: 'Library',
+        book: req.book
+      });
     });
   return bookRouter;
 }
